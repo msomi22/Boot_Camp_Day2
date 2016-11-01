c42722e9258a91aabbf269c1b42c7b1a283e1fb6
msomi22 <mwendapeter72@gmail.com>
2016-11-01 12:38:18 +0300

    A well commented code

--- sendSms.py
+++ sendSms.py
@@ -1,12 +1,19 @@
-# Sending SMS using SMS- API
+# Sending SMS using AfricasTalkingGateway SMS- API
 from AfricasTalkingGateway import AfricasTalkingGateway, AfricasTalkingGatewayException
 class SendSMS(object):
+	"""
+	Attributes:
+	username: The API username requied for sending SMS
+	apikey: A key required for sending SMS
+	recipients: The recipients mobile numbers , ie 0718953974
+    message: The message to send
+	"""
 	def __init__(self,username='',apikey='',recipients='',message=''):
 		self.username = username
 		self.apikey = apikey
 		self.recipients = recipients
 		self.message = message
-
+    # The method thet is called to send the SMS
 	def sendNow(self):
 		gateway = AfricasTalkingGateway(self.username, self.apikey)
 		try:
@@ -22,11 +29,13 @@ class SendSMS(object):
 			print 'Encountered an error while sending the SMS: %s' % str(e)
 		
 
-
+# Prompt the user to enter the required credentials 
 username = raw_input("Enter Your API username: ")
 apikey   = raw_input("Enter Your API Key: ")
 recepients  = raw_input("Enter Your Recepient, separate with comma for multiple Recepients: ")
 message = raw_input("Type Your Message: ")
 
+# Instantiate the sendSMS class
 send = SendSMS(username,apikey,recepients,message)
+#Invoke the send sms function 
 send.sendNow() 
